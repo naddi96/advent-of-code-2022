4c4e3f998bf16182bf633fc7c43c6ba8ac512e92
naddi96 <nardidamiano@gmail.com>
2022-12-05 08:02:52 +0100

    Clean up ignored files

--- utility/api.py
+++ utility/api.py
@@ -2,9 +2,13 @@ import http.client
 from datetime import date
 import os
 
-
-with open(f"{os.path.dirname(os.path.abspath(__file__))}/session_cookie.txt","r")as f:
-    session=f.read()
+try:
+    with open(f"{os.path.dirname(os.path.abspath(__file__))}/session_cookie.txt","r")as f:
+        session=f.read()
+except:
+    with open(f"{os.path.dirname(os.path.abspath(__file__))}/session_cookie.txt","w")as f:
+        f.write("put the session cokie here")
+    
 
 def get_input(session_cookie=session
 ,year=date.today().year, day=date.today().day):
